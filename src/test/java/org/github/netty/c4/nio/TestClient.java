ff73373a4ac9362cdc2afe75905ca634c84ebe4d
Master-He <478243824@qq.com>
2022-06-09 23:55:37 +0800

    TestServer，TestClient

---
+++ src/test/java/org/github/netty/c4/nio/TestClient.java
@@ -0,0 +1,35 @@
+package org.github.netty.c4.nio;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TestClient {
+    // 回顾nio内容
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost", 8888);
+
+        new Thread(()->{
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                while (true) {
+                    System.out.println(reader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                for (int i = 0; i < 100; i++) {
+                    writer.write(String.valueOf(i));
+                    writer.newLine();
+                    writer.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
