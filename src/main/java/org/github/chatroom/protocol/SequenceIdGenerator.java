1108620743d9f70d32eff2ce61f17ac271c29bf1
Master-He <478243824@qq.com>
2022-06-14 00:38:21 +0800

    Rpc框架

---
+++ src/main/java/org/github/chatroom/protocol/SequenceIdGenerator.java
@@ -0,0 +1,11 @@
+package org.github.chatroom.protocol;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class SequenceIdGenerator {
+    private static final AtomicInteger id = new AtomicInteger();
+
+    public static int nextId() {
+        return id.incrementAndGet();
+    }
+}
---
+++ src/test/java/org/github/nettyadvance/c3/TestGson.java
@@ -0,0 +1,12 @@
+package org.github.nettyadvance.c3;
+
+import org.github.chatroom.protocol.Serializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class TestGson {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().registerTypeAdapter(Class.class, new Serializer.ClassCodec()).create();
+        System.out.println(gson.toJson(String.class));
+    }
+}
